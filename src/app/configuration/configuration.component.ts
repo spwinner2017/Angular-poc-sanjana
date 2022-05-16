1a5a519c9a59d0e372bcdc61462ed5304c911c3d
Sarjerao Ghadage <sarjerao.ghadage@ihsmarkit.com>
2022-05-16 23:25:51 +0530

    Initial commit

---
+++ src/app/configuration/configuration.component.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+
+@Component({
+  selector: 'app-configuration',
+  templateUrl: './configuration.component.html',
+  styleUrls: ['./configuration.component.css']
+})
+export class ConfigurationComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) { }
+  columnDefs: ColDef[] = [
+    { field: 'id' },
+    { field: 'name' },
+    { field: 'country' },
+    { field: 'established' },
+    { field: 'website' },
+    { field: 'logo' },
+    { field: 'head_quaters' }
+  ];
+
+  rowData = [];
+  ngOnInit() {
+    this.loadData();
+  }
+  loadData() {
+    let headers = new HttpHeaders();
+    headers.set('Access-Control-Allow-Headers', 'Content-Type');
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    headers.set('Access-Control-Allow-Origin', '*');
+    //  : Content-Type, X-Requested-With, X-authentication, X-client
+    // http://dummy.restapiexample.com/api/v1/employees
+    const path = 'https://api.instantwebtools.net/v1/passenger?page=0&size=50'
+    this.httpClient.get(path).subscribe((rec: any) => {
+      console.table(rec);
+
+      const data = [];
+      rec.data.forEach(element => {
+        data.push(element.airline[0]);
+      });
+      this.rowData = data;
+      console.table(data);
+    })
+  }
+  onGridReady(event) {
+    console.log('onGridReady executed');
+    event.columnApi.autoSizeAllColumns();
+  }
+}
